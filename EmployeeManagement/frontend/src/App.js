c76a6f61dd4c1baff12dff4aa2482c0c2bc9582d
Malinda <it21807862@my.sliit.lk>
2023-08-31 22:39:09 +0530

    new code

---
+++ EmployeeManagement/frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login,Signup } from './Routes.js';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
