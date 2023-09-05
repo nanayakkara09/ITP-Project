bc3d86389449c4db81db94ea7bd3549f802edb60
dihan01 <142965252+dihan01@users.noreply.github.com>
2023-09-05 11:41:50 +0530

    itemlist

---
+++ login/client/vite-project/src/pages/addNew.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './addNew.css'; // Import the CSS file for styling
+
+export default function NewItemForm() {
+  const [formData, setFormData] = useState({
+    category: 'Select a category',
+    itemcode: '',
+    name: '',
+    description: '',
+    quantity: '',
+    reorder: '',
+    
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    console.log(formData)
+    const{name,description,quantity,category,reorder,itemcode}= formData 
+    const {data} =await axios.post('./inventory/',{name,description,quantity,reorder,itemcode})
+    console.log('Form data submitted:', formData);
+  };
+
+  return (
+    <div className="new-item-form-container">
+      <h2>Add New Item</h2>
+      <form onSubmit={handleSubmit}>
+      <div className="form-group">
+          <label htmlFor="category">Category:</label>
+          <select
+            id="category"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+          >
+            <option value="Select a category">Select a category</option>
+            <option value="Category 1">Food</option>
+            <option value="Category 2">Furniture</option>
+            <option value="Category 3">Machinery</option>         
+         </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="itemcode">Item Code:</label>
+          <input
+            type="text"
+            id="itemCode"
+            name="itemcode"
+            value={formData.itemCode}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description:</label>
+          <textarea
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="quantity">Quantity:</label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            value={formData.quantity}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="reorder">Reorder Level:</label>
+          <input
+            type="number"
+            id="reorder"
+            name="reorder"
+            value={formData.reorder}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <button type="cancel">Cancel</button>
+      </form>
+    </div>
+  );
+}
