d140257d356645bc7a00e7b210147e9c6416fdd6
dihan01 <142965252+dihan01@users.noreply.github.com>
2023-09-19 21:38:00 +0530

    Update admin-dashbord.jsx

--- login/client/vite-project/src/pages/admin-dashbord.jsx
+++ login/client/vite-project/src/pages/admin-dashbord.jsx
@@ -1,16 +1,15 @@
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../contex/userContex";
 import axios from "axios";
-import { Link, useNavigate } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import AdminNavBar from '../components/adminNavBar'; 
-import './admin-dashbord.css';
-// Import custom CSS file for styling
+import { Link, useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import AdminNavBar from "../components/adminNavBar";
+import "./admin-dashbord.css";
 
-export default function admindashbord() {
+export default function AdminDashboard() {
   const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
+  const [showDropdown, setShowDropdown] = useState(false); // State to manage dropdown visibility
 
   useEffect(() => {
     if (!user) {
@@ -22,35 +21,65 @@ export default function admindashbord() {
 
   const handleLogout = async () => {
     try {
-      await axios.post('/logout');
+      await axios.post("/logout");
       setUser(null);
-      navigate('/login');
+      navigate("/login");
     } catch (error) {
       console.log(error);
     }
   };
 
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <div>
-        <AdminNavBar />
-       
+      <AdminNavBar />
+
       <div className="bg-image"></div>
-      <div className='content'>
+      <div className="content">
         <div className="black-box">
           <br></br>
           <div className="vertical-text">Customer</div>
-        <div className="vertical-text">Event</div>
-        <div className="vertical-text">Stall</div>
-        <div className="vertical-text">Employee</div>
-        <div className="vertical-text">Order</div>
-        <div className="vertical-text">Delivery</div>
-        <div className="vertical-text">Inventory</div>
-        <div className="vertical-text">Finance</div>
+          
+          <div className="vertical-text">Order</div>
+          <div className="vertical-text">Delivery</div>
+          <div className={`dropdown ${showDropdown ? "show" : ""}`}>
+            <button
+              type="button"
+              className="btn btn-secondary dropdown-toggle"
+              onClick={toggleDropdown}
+            >
+              Inventory
+            </button>
+            <div className={`dropdown-menu ${showDropdown ? "show" : ""}`}>
+              <Link className="dropdown-item" to="/itemlist">
+                Food
+              </Link>
+              <Link className="dropdown-item" to="/inventory-page-2">
+                Furniture
+              </Link>
+              <Link className="dropdown-item" to="/inventory-page-3">
+                Machinery
+              </Link>
+              <Link className="dropdown-item" to="/inventory-page-4">
+                Issued
+              </Link>
+              <Link className="dropdown-item" to="/inventory-page-5">
+                Reports
+              </Link>
+              {/* Add more dropdown items as needed */}
+            </div>
+          </div>
+          <div className="vertical-text">Finance</div>
+          <div className="vertical-text">Event</div>
+          <div className="vertical-text">Stall</div>
+          <div className="vertical-text">Employee</div>
         </div>
         <br></br>
-        {!!user && <h2>welcome {user.name}!</h2>}
+        {!!user && <h2>Welcome, {user.name}!</h2>}
         <br></br>
-        
       </div>
       <footer className="footers">
         <div className="container">
