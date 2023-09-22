e70a57f66394656ab9872b04713b42c87e02f373
IT21806322 <it21806322@my.sliit.lk>
2023-09-22 22:17:51 +0530

    Create eventRoutes.js
    eventRoutes

---
+++ login/server/routes/eventRoutes.js
@@ -0,0 +1,24 @@
+const express =require('express');
+const router = express.Router();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const{event,eventUpdate,eventDelete,eventSuccess,eventAbout,eventDetail,eventHome}=require('../controllers/invController')
+
+//middleware
+router.use(
+    cors({
+        credentials:true,
+        origin:'http://localhost:5173'
+    })
+)
+
+router.get('/event',event);
+router.put('/eventUpdate',eventUpdate);
+router.delete('/eventDelete',eventDelete);
+router.post('/eventSuccess',eventSuccess);
+router.post('/eventAbout',eventAbout);
+router.post('/eventDetail',eventDetail);
+router.post('/eventHome',eventHome);
+
+module.exports=router
