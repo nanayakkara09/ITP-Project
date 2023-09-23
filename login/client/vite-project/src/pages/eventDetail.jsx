beacc89688cb30a4136390d811b5cba085d0ea70
IT21806322 <it21806322@my.sliit.lk>
2023-09-23 11:11:35 +0530

    detail

--- login/client/vite-project/src/pages/eventDetail.jsx
+++ login/client/vite-project/src/pages/eventDetail.jsx
@@ -5,44 +5,48 @@ import { useNavigate } from 'react-router-dom';
 export default function eventDetail() {
   const navigate = useNavigate();
   return (
-    <div className="event-table">
+    <div className="event-details-container">
       <div className="content">
         <h2>SEASONAL OFFERS!</h2>
-        <div>
-      <h2>EVENT PROPOSAL DETAILS</h2>
-      <table className="event-table">
-      <ul>
-        <li>
-          <strong>Number of crowd expected:</strong> 50
-        </li>
-        <li>
-          <strong>Theme:</strong> Black and white
-        </li>
-        <li>
-          <strong>Event time:</strong> Night function
-        </li>
-        <li>
-          <strong>Event location:</strong> Outdoor
-        </li>
-        <li>
-          <strong>Extra details:</strong>
-          <ul>
-            <li>DJ and Disco</li>
-            <li>Furniture Hire</li>
-            <li>Styling & Decor</li>
-            <li>Food and Beverages</li>
-            <li>Liquor</li>
+        <div className="event-proposal-details">
+          <h2>EVENT PROPOSAL DETAILS</h2>
+          <ul className="event-table">
+            <li>
+              <strong>Number of crowd expected:</strong> 50
+            </li>
+            <li>
+              <strong>Theme:</strong> Black and white
+            </li>
+            <li>
+              <strong>Event time:</strong> Night function
+            </li>
+            <li>
+              <strong>Event location:</strong> Outdoor
+            </li>
+            <li>
+              <strong>Extra details:</strong>
+              <ul>
+                <li>DJ and Disco</li>
+                <li>Furniture Hire</li>
+                <li>Styling & Decor</li>
+                <li>Food and Beverages</li>
+                <li>Liquor</li>
+              </ul>
+            </li>
+            <li>
+              <strong>Budget:</strong> RS.60000/=
+            </li>
           </ul>
-        </li>
-        <li>
-        <strong>Budget:</strong> RS.60000/=
-        </li>
-      </ul></table>
-      <button onClick={() => navigate('/eventSuccess')} type="submit" className="btn btn-primary">
-           Submit
+          <button
+            onClick={() => navigate('/eventSuccess')}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Submit
           </button>
-    </div>
+        </div>
       </div>
+  
       <hr />
       <footer className="footer">
         <div className="container">
