bc06e6b3d8c3851a3f78967a3f2cf2362841cc8a
Naduka Kawya <111105542+nanayakkara09@users.noreply.github.com>
2023-09-27 12:13:28 +0530

    Revert "updated"
    This reverts commit 1bbaeb3088ee5aeb89533cf585df05a8189e1681.

--- login/client/vite-project/src/pages/support.jsx
+++ login/client/vite-project/src/pages/support.jsx
@@ -5,7 +5,12 @@ import './feedbacks.css';
 
 export default function SupportMessage() {
   const [supportText, setSupportText] = useState('');
-  const [userName, setUserName] = useState(''); // State for user name
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const validateEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
 
   const submitSupport = async (e) => {
     e.preventDefault();
@@ -15,16 +20,23 @@ export default function SupportMessage() {
       return;
     }
 
+    if (!validateEmail(email)) {
+      toast.error('Invalid email address');
+      return;
+    }
+
     try {
       await axios.post('/submitsupport', {
         userId: 'user_id_here',
+        email,
+        userName,
         supportText,
-        userName, // Include the user name in the request
       });
 
-      toast.success('Support message submitted successfully');
+      toast.success('Support message submitted successfully,Our Agents Will Email you Soon!!');
       setSupportText('');
-      setUserName(''); // Clear the user name input after submission
+      setUserName('');
+      setEmail('');
     } catch (error) {
       console.error(error);
       toast.error('Error submitting support message');
@@ -36,9 +48,9 @@ export default function SupportMessage() {
       <div className="container">
         <br />
         <br />
-        <h1 className="h1-responsive font-weight-bold text-center my-4">
+        <h2 className="h1-responsive font-weight-bold text-center my-4">
           Submit Support Message
-        </h1>
+        </h2>
         <div className="feedback-form">
           <form onSubmit={submitSupport}>
             <div className="md-form">
@@ -49,6 +61,23 @@ export default function SupportMessage() {
                 placeholder="Your Name"
                 value={userName}
                 onChange={(e) => setUserName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="md-form">
+              <input
+                type="text"
+                id="email"
+                className="form-control"
+                placeholder="Your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onBlur={(e) => {
+                  const emailValue = e.target.value;
+                  if (!validateEmail(emailValue)) {
+                    toast.error('Invalid email address');
+                  }
+                }}
                 required
               />
             </div>
