5ca32aa42b491f948e99c4092ba5bca4216ec0a8
Anjana Tharanga <129808844+AnjanaTharanga24@users.noreply.github.com>
2023-09-28 07:57:10 +0530

    insert function

--- login/client/vite-project/src/pages/PizzaMart.jsx
+++ login/client/vite-project/src/pages/PizzaMart.jsx
@@ -1,16 +1,72 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Card, Form, InputGroup } from 'react-bootstrap';
+import React, { useState , useEffect} from 'react';
+import { Container, Row, Col, Card, Form, InputGroup, Button,Modal } from 'react-bootstrap';
 import ReactStars from 'react-rating-stars-component';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const PizzaMart = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('All');
   const { shopId } = useParams();
   const [cart, setCart] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [itemQuantities, setItemQuantities] = useState(1);
+
+  const handleShowModal = (item) => {
+    setSelectedItem(item);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+
+  const handleQuantityChange = (item, action) => {
+    const updatedQuantities = { ...itemQuantities };
+
+    const index = stall.foodItems.findIndex(i => i.name === item.name);
+
+    if (action === 'increase') {
+      updatedQuantities[index] = (updatedQuantities[index] || 0) + 1;
+    } else if (action === 'decrease' && updatedQuantities[index] > 1) {
+      updatedQuantities[index] -= 1;
+    }
+
+    setItemQuantities(updatedQuantities);
+    localStorage.setItem('itemQuantities', JSON.stringify(updatedQuantities));
+
+    
+  };
+
+  const loadItemQuantities = () => {
+    const storedQuantities = JSON.parse(localStorage.getItem('itemQuantities'));
+    return storedQuantities || {};
+  };
+
+  useEffect(() => {
+    setItemQuantities(loadItemQuantities());
+  }, []);
+  
+  const calculateTotalPrice = (item, quantity) => {
+    return  item.price * quantity;
+  };
+
+  
 
   const addToCart = (item) => {
-    setCart([...cart, item]);
+    const { name, price, image } = item;
+    const quantity = itemQuantities[filteredFoodItems.indexOf(item)] || 1;
+  
+    axios.post('http://localhost:8000/order/add-to-cart', { name, quantity, price, image })
+    .then((response) => {
+      alert(response.data.message);
+    })
+    .catch((error) => {
+      console.error(error);
+      alert('Error adding item to cart');
+    });
+  
   };
 
   // Sample stall data (replace with your actual data)
@@ -123,8 +179,40 @@ const PizzaMart = () => {
                         />
                       </div>
                     </Card.Text>
-                    <p>Price: {item.price} LKR</p>
-                    <button onClick={() => addToCart(item)}>Add to Cart</button>
+
+                    <div>
+                    <p> Price: ${calculateTotalPrice(item, itemQuantities[index] || 0)}</p>
+
+                      
+                    <div className='quantity-control'>
+                        <Button
+                       variant="outline-success"
+                       onClick={() => handleQuantityChange(item, "increase")}
+                        >
+                            +
+                        </Button>
+
+                        <span className="quantity">{itemQuantities[index] || 1}</span>
+                          <Button
+                           variant="outline-danger"
+                           onClick={() => handleQuantityChange(item, "decrease")}
+                          >
+                            -
+
+                          </Button>
+
+                    </div>
+
+                    </div>
+
+                  
+
+                      <Button className='addToCart' variant="success" 
+                      onClick={() => addToCart(item)}
+                      >
+                        Add to Cart
+                      </Button>
+                    
                   </Card.Body>
                 </Card>
               </Col>
@@ -132,6 +220,8 @@ const PizzaMart = () => {
           </Row>
         </Col>
       </Row>
+
+      
     </Container>
   );
 };
