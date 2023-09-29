4384d1979a6dc1363db422a0db74073f65e3fb99
Thilinaxxx <pererathilina2001@gmail.com>
2023-09-29 17:13:17 +0530

    add

--- login/server/routes/OrderRoute.js
+++ login/server/routes/OrderRoute.js
@@ -22,6 +22,25 @@ router.post('/add-to-cart', async (req, res) => {
 });
 
 
+// Define a route to fetch order details by ID
+router.get('/orders/:id', async (req, res) => {
+  try {
+    const orderId = req.params.id;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+
+    res.status(200).json({ message: 'Order fetched successfully', order });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
 
 
 
