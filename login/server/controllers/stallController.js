b2714754a33916fc8128e2b6c04b3f2f5d671106
Thilinaxxx <pererathilina2001@gmail.com>
2023-10-11 02:33:05 +0530

    sdsd

--- login/server/controllers/stallController.js
+++ login/server/controllers/stallController.js
@@ -1,7 +1,101 @@
 const Stall = require('../models/stall');
 
+<<<<<<< HEAD
 const test = (req, res) => {
     res.json('test is working');
+=======
+
+const createStall = async (req, res) => {
+    try{
+       const {stallName, type, amount, mType,description, stallId, fName, lName, phonenumber, email, password, payment} = req.body;
+       // Check if name was entered
+       if (!stallName) {
+        return res.json({
+            error: 'Name is required'
+        });
+    };
+
+       if (!password || password.length < 6){
+        return res.json({
+          error: 'Password is required and should be at least 6 characters long'
+       });
+       };
+
+       //check email
+       const exist = await StallRegister.findOne({email});
+       if (exist) {
+        return res.json({
+          error: 'Email is taken already'
+        })
+       }  
+
+       const hashedPassword = await hashPassword(password); 
+      const createStallResult = await StallRegister.create({
+        stallName,
+        type,
+        amount,
+        mType,
+        description,
+        stallId,
+        fName,
+        lName,
+        phonenumber,
+        email,
+        password: hashedPassword,
+        payment
+      })
+
+      return res.json(createStallResult)
+
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error creating a stall' });
+    }
+
+} ;
+
+//Login Endpoint
+const Stalllogin = async (req, res) => {
+  try{
+       const {email, password} = req.body;
+
+       //Check if user exist
+       const  stall = await StallRegister.findOne({email});
+       if(!stall){
+        return res.json({
+          error: 'No Stall found'
+        })
+       }
+
+       //Check if password match
+       const match = await comparePassword(password, stall.password)
+       if (match) {
+        jwt.sign({email:stall.email, id:stall._id, fName: stall.fName}, process.env.jwt_SECRET, {}, (err, token) => {
+          if (err) throw err;
+          res.cookie('token', token).json(stall)
+        } )
+       }
+       if(!match){
+        res.json({
+          error: ("Password do not match")
+        })
+       }
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+const StallOwnerDashboard = (req, res) => {
+const {token}  = req.cookies
+if(token) {
+  jwt.verify(token, process.env.JWT_SECRET,{}, (err, stall) => {
+    if(err) throw err;
+    res.json(stall)
+  })
+} else {
+  res.json(null)
+}
+>>>>>>> parent of 57f6d443e (Stall Registration Payment default value)
 }
 
 const stallreq = async (req, res) => {
