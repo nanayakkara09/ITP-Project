fe58054057fd75feae50c7b0b32617a1d6edf0f3
IT21806322 <it21806322@my.sliit.lk>
2023-10-16 06:40:02 +0530

    update

--- login/client/vite-project/src/pages/eventHome.jsx
+++ login/client/vite-project/src/pages/eventHome.jsx
@@ -84,13 +84,13 @@ Event branding.</span>
           </div>
           <hr />
           <div className="button-container">
-            <button onClick={() => navigate('/eventDetail')} className="btn btn-primary">
+            <button onClick={() => navigate('/eventDetail')} className="heshanibtn btn-primary">
               Event Details
             </button>
-            <button onClick={() => navigate('/addNewEvent')} className="btn btn-primary">
+            <button onClick={() => navigate('/addNewEvent')} className="heshanibtn btn-primary">
              Add Event
             </button>
-            <button onClick={() => navigate('/eventAbout')} className="btn btn-primary">
+            <button onClick={() => navigate('/eventAbout')} className="heshanibtn btn-primary">
               About Us
             </button>
             </div> 
@@ -103,19 +103,15 @@ Event branding.</span>
    
       
       <hr />
-      <footer className="footer">
+      <footer className="heshafooter25">
+      <div className="heshanicard25">
         <div className="container">
           <div className="contact-section">
             <h4>Contact Us</h4>
             <p>Phone: 123-456-7890</p>
             <p>Email: info@foodstore.com</p>
-          </div>
-          <div className="social-section">
-            {/* Add your social media links here */}
-          </div>
-          <hr />
-          <p className="text-center">All rights reserved &copy; </p>
-        </div>
+        <p className="heshanitext-center">All rights reserved &copy;</p>
+        </div></div> </div>
       </footer>
     </div>
     </div> </div></div>
